d3cf4b63972402b2356e25cbeb596403798d2176
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-07-28 20:49:24 +0600

    so far done

--- src/api/index.js
+++ src/api/index.js
@@ -4,20 +4,26 @@ import axios from 'axios'
 
 const url = 'https://covid19.mathdro.id/api'
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let changeableUrl = url
+
+    if(country) {
+        changeableUrl = `${url}/countries/${country}`
+    }
+
     try {
-        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url)
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl)
 
         return { confirmed, recovered, deaths, lastUpdate }
 
     } catch (error) {
-        return error
+        console.log(error);
     }
 }
 
 export const fetchDailyData = async () => {
     try {
-        const { data } = await axios.get(`https://covid19.mathdro.id/api/daily`)
+        const { data } = await axios.get(`${url}/daily`)
 
         const modifiedData = data.map((dailyData) => ({
             confirmed: dailyData.confirmed.total,
@@ -26,6 +32,17 @@ export const fetchDailyData = async () => {
         }))
        return modifiedData
     } catch (error) {
-        return error
+        console.log(error);
     }
 } 
+
+export const fetchCountries = async () => {
+    try {
+        const {data : {countries}} = await axios.get(`${url}/countries`)
+
+        return countries.map((country)=>  country.name)
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Line, Bar } from 'react-chartjs-2';
 import { fetchDailyData } from '../../api'
 
-
 import styles from './Chart.module.css'
 
-const Chart = () => {
+const Chart = ({data: {confirmed, recovered,  deaths}, country}) => {
     
     const [dailyData, setDailyData] = useState([])
 
@@ -43,9 +40,33 @@ const Chart = () => {
 
     )
 
+    const barChart = (
+        confirmed
+        ? (
+            <Bar
+            data={{
+                labels: ['Infected', 'Recover', 'Deaths'],
+                datasets:[{
+                    label: 'People',
+                    backgroundColor:[
+                        'rgba(0, 0, 255, 0.5)',
+                        'rgba(0, 255, 0, 0.5)',
+                        'rgba(255, 0, 0, 0.5)'
+                    ],
+                    data:[confirmed.value, recovered.value, deaths.value]
+                }]
+            }}
+            options={{
+               legend:{display: false},
+                title: {display: true, text: `Current state in ${country}`}
+            }}
+            />
+        ) : null
+    )
+
     return (
         <div className={styles.container}>
-            {lineChart}
+            {country ? barChart : lineChart}
         </div>
     );
 };
