98b33dac7fea5ac94e8997bcfd6f6a48cc55a6a9
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-07-28 15:02:21 +0600

    api set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import React from 'react'
+import styles from './App.module.css'
 
-function App() {
-  return (
-    <div>
-      <h1>Covid Tracker</h1>
-    </div>
-  );
+import { Cards, Chart, CountryPicker } from './components'
+import { fetchData } from './api'
+
+
+class App extends React.Component {
+  state = {
+    data: {},
+  }
+  async componentDidMount() {
+    const fetchedData = await fetchData()
+    this.setState({ data: fetchedData })
+  }
+  render() {
+    const { data } = this.state
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
